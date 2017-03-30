f0549fcd25fbce8f9c1de061474e5bf76ea56b2e
Basara.Wu(吳欣昌) <Basara.Wu@TCI-BIO.com>
2017-03-30 18:13:49 +0800

    change vue cli use webpack

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
 
+Vue.config.productionTip = false
+Vue.prototype.$http = Axios;
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  template: '<App/>',
+  components: { App }
 })
